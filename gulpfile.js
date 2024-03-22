46265cf1490980979fc4b36c9bc1308e627678d6
Aniket Sharma <aniketsharma2468@gmail.com>
2024-03-22 08:35:28 +0530

    version updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,112 +1,78 @@
-var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    connect = require('gulp-connect'),
-    less = require('gulp-less'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
-    rimraf = require('gulp-rimraf')
-watch = require('gulp-watch');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const connect = require('gulp-connect');
+const less = require('gulp-less');
+const minifyCSS = require('gulp-minify-css');
+const rename = require('gulp-rename');
+const del = require('del'); // Switched from rimraf to del
 
-var paths  = {
-    fonts: 'fonts/**.*',
-    images: 'img/**/*.*',
-    styles: 'less/**/*.less',
+const paths = {
+  fonts: 'fonts/**.*',
+  images: 'img/**/*.*',
+  styles: 'less/**/*.less',
 };
 
-/**
- * Watch src and execute tasks when changes are made
- */
-gulp.task('watch', function() {
-    gulp.watch([paths.styles], ['less', 'livereload']);
-    gulp.watch([paths.fonts], ['fonts', 'livereload']);
-});
+function clean() {
+  return del(['dist']);
+}
 
-/*
- * Serve the files out of /dist
- */
-gulp.task('connect', function() {
-    connect.server({
-        root: 'dist',
-        livereload: true,
-        port: 8888
-    });
-});
+function cleanCss() {
+  return del(['dist/css/*']);
+}
 
-/*
- * Reload the web server
- */
-gulp.task('livereload', function() {
-    gulp.src('index.html')
-        .pipe(connect.reload());
-});
+function cleanImages() {
+  return del(['dist/img/*']);
+}
 
-/*
- * Cleans the distribution directory
- */
-gulp.task('clean', ['clean-css', 'clean-images', 'clean-fonts', 'clean-dist']);
+function cleanFonts() {
+  return del(['dist/fonts/*']);
+}
 
-gulp.task('clean-dist', function() {
-    return gulp.src('dist', {
-            read: false
-        })
-        .pipe(rimraf({
-            force: true
-        }));
-});
+function fonts() {
+  return gulp.src(paths.fonts)
+    .pipe(gulp.dest('dist/fonts'));
+}
 
-gulp.task('clean-css', function() {
-    return gulp.src('dist/css/*', {
-            read: false
-        })
-        .pipe(rimraf({
-            force: true
-        }));
-});
+function compileLess() {
+  return gulp.src(paths.styles)
+    .pipe(less())
+    .pipe(concat('rdash.css'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(minifyCSS())
+    .pipe(rename('rdash.min.css'))
+    .pipe(gulp.dest('dist/css'));
+}
 
-gulp.task('clean-images', function() {
-    return gulp.src('dist/img/*', {
-            read: false
-        })
-        .pipe(rimraf({
-            force: true
-        }));
-});
+function livereload() {
+  return gulp.src('*.html')
+    .pipe(connect.reload());
+}
 
-gulp.task('clean-fonts', function() {
-    return gulp.src('dist/fonts/*', {
-            read: false
-        })
-        .pipe(rimraf({
-            force: true
-        }));
-});
+function watchTasks() {
+  gulp.watch(paths.styles, gulp.series(compileLess, livereload));
+  gulp.watch(paths.fonts, gulp.series(fonts, livereload));
+}
 
-/*
- * Copy fonts to dist directory
- */
-gulp.task('fonts', function() {
-    return gulp.src(paths.fonts)
-        .pipe(gulp.dest('dist/fonts'));
-});
+function connectServer() {
+  return connect.server({
+    root: 'dist',
+    livereload: true,
+    port: 8888
+  });
+}
 
-/**
- * Compile less
- */
-gulp.task('less', function() {
-    gulp.src(paths.styles)
-        .pipe(less())
-        .pipe(concat('rdash.css'))
-        .pipe(gulp.dest('dist/css'));
+const build = gulp.series(
+  gulp.parallel(cleanCss, cleanImages, cleanFonts, clean),
+  gulp.parallel(fonts, compileLess)
+);
 
-    return gulp.src('dist/css/rdash.css')
-        .pipe(minifycss())
-        .pipe(rename('rdash.min.css'))
-        .pipe(gulp.dest('dist/css/'))    
-});
+const watch = gulp.parallel(watchTasks, connectServer);
 
-/*
- * Copy assets and compile less.
- */
-gulp.task('build', ['fonts', 'less']);
-
-gulp.task('default', ['build', 'connect', 'livereload', 'watch']);
+exports.clean = clean;
+exports.fonts = fonts;
+exports.less = compileLess;
+exports.livereload = livereload;
+exports.watch = watchTasks;
+exports.connect = connectServer;
+exports.build = build;
+exports.default = gulp.series(build, watch);
